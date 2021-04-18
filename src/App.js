75c331211a8902dac86f6818b8be6278615770f1
mt1221 <makotonokotodesu@gmail.com>
2021-04-18 14:01:56 +0900

    change use props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
 
 function App() {
+  const profiles = [
+    { name: "Taro", age: 10},
+    { name: "Hanako", age: 5},
+    { name:"noName"}
+  ]
   return (
     <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("I am clicked!")}}></input>
+      {
+          profiles.map((profile) => {
+            return <User name={profile.name} age={profile.age}/>
+          })
+      }
     </React.Fragment>
   )
   
 }
 
+function User(props) {
+  return <div>Hi, I am {props.name}, and {props.age} years old</div>
+}
+
+User.defaultProps = {
+  age:1
+}
 
 export default App;
