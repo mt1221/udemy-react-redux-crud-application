134267cd6f702e1404f92481c64b498ef21e2bf3
mt1221 <makotonokotodesu@gmail.com>
2021-04-18 21:41:12 +0900

    change reducer and actions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { provider } from 'react-redux'
+
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <provider store={store}>
+      <App />
+    </provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
