f9a88859c954bdb1bfdeb6f43a07a3f4e81ec384
mt1221 <makotonokotodesu@gmail.com>
2021-04-18 22:05:13 +0900

    complete redux-basics

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -2,13 +2,15 @@ import { INCREMENT, DECREMENT } from "../actions"
 
 const initialState = { value: 0 }
 
-export default (state = initialState, action) => {
+const switchEvents = (state = initialState, action) => {
     switch (action.type) {
         case INCREMENT:
             return { value: state.value + 1}
         case DECREMENT:
             return { value: state.value - 1}
-        defaul: 
+        default:
             return state
     }
 }
+
+export default switchEvents;
